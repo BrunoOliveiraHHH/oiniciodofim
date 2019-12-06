d5d53090b7a91d7fd09f5125d1ca895102d6bd8d
BrunoOliveiraHHH <bruno.d.l.oliveira@outlook.com>
2019-12-06 10:58:43 -0300

    First Commit

---
+++ src/main/java/com/iniciodofim/iniciodofim/IniciodofimApplication.java
@@ -0,0 +1,13 @@
+package com.iniciodofim.iniciodofim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IniciodofimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IniciodofimApplication.class, args);
+	}
+
+}
